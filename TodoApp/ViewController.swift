4a297cae9abcd9e7e61e500c6c7b12cc7070ad69
kobayashi-kenta <12321klj@gmail.com>
2021-01-05 22:53:14 +0900

    画面遷移、デリゲートの実装

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+  
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = "左に表示"
+        cell?.detailTextLabel?.text = "右に表示"
+        
+        return cell!
+    }
+    
+    
 
 }
 
